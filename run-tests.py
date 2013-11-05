b1c0dbbffad5856595b687e033de46a4d04cddf7
Barnaby Court <bcourt@redhat.com>
2013-11-05 16:48:45 -0500

    Make run-tests scripts directory safe

--- run-tests.py
+++ run-tests.py
@@ -31,4 +31,7 @@ TESTS = [
 
 PLUGIN_TESTS = ['pulp_puppet_plugins/test/unit/',]
 
-run_tests(PACKAGES, TESTS, PLUGIN_TESTS)
+dir_safe_all_platforms = [os.path.join(os.path.dirname(__file__), x) for x in TESTS]
+dir_safe_non_rhel5 = [os.path.join(os.path.dirname(__file__), x) for x in PLUGIN_TESTS]
+
+run_tests(PACKAGES, dir_safe_all_platforms, dir_safe_non_rhel5)
